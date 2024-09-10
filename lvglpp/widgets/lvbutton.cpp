19e21ff690115b7c204f363a21d8d99afcd22e89
nghiatd <tranduynghia2002@gmail.com>
2024-09-10 11:55:59 +0700

    feat: update LvButton class

---
+++ lvglpp/widgets/lvbutton.cpp
@@ -0,0 +1,46 @@
+#include "lvbutton.h"
+
+LvButton::LvButton(LvBaseObject *parent, QString text) :
+    LvBaseObject(parent), mContent(text)
+{
+    mButtonPressedEffectTimer = new QTimer();
+    mButtonPressedEffectTimer->setSingleShot(true);
+    mButtonPressedEffectTimer->setInterval(100);
+
+    connect(mButtonPressedEffectTimer, &QTimer::timeout, this, &LvButton::onButtonPressedEffectTimerHandler);
+}
+
+void LvButton::create()
+{
+    isCreated = true;
+    if (mParent)
+        mLvObj = lv_button_create(mParent->getLvObject());
+    else
+        mLvObj = lv_button_create(lv_screen_active());
+
+    mContentLabel = lv_label_create(mLvObj);
+    lv_label_set_text(mContentLabel, mContent.toStdString().c_str());
+    lv_obj_center(mContentLabel);
+}
+
+void LvButton::press()
+{
+    if (!isCreated)
+    {
+        qDebug() << "[Warning] button was not created!";
+        return;
+    }
+    mButtonPressedEffectTimer->start();
+    this->setBgOpa(LV_OPA_0);
+    emit pressed();
+}
+
+void LvButton::onButtonPressedEffectTimerHandler()
+{
+    if (!isCreated)
+    {
+        qDebug() << "[Warning] button was not created!";
+        mButtonPressedEffectTimer->stop();
+    }
+    this->setBgOpa(LV_OPA_100);
+}
---
+++ lvglpp/widgets/lvline.h
@@ -0,0 +1,11 @@
+#ifndef LVLINE_H
+#define LVLINE_H
+
+
+class LvLine
+{
+public:
+    LvLine();
+};
+
+#endif // LVLINE_H
